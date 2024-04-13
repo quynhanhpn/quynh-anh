b25db57188ee1229d15add27a6da0a6ed90c7c4a
quynhanhnp <qanh21032007@gmail.com>
2024-04-13 17:43:29 +0700

    first commit

---
+++ a.py
@@ -0,0 +1,67 @@
+#bài 1
+# r = float(input("Radius: "))
+# pi = float(3.1416)
+# print( float(2*pi*r ))
+# print(float(pi*r**2))
+#bài 2
+# import math
+# x = math.sqrt(2)
+# a = int(input("Length of edge: "))
+# print(a*x)
+#bài 3
+# Accountname = input("Account name: ")
+# Domainname = input("Domain name: ")
+# print(Accountname+"@"+Domainname)
+#bài 4
+# day = input("Date in MM/DD/YYYY format: ")
+
+
+
+#bài 5
+# a =float( input("Deposit: "))
+# print( a*105/100)
+# print(a*(105/100)**2)
+# print(a*(105/100)**10)
+#bài 6
+# from turtle import*
+# forward(200)
+# left(90)  
+# pencolor('#de5246')  
+# forward(150)
+# left(125)
+# forward(125)
+# left(-70)
+# forward(125)
+# left(125)
+# forward(150)
+# penup()
+# goto(0,150)
+# pencolor('#000000')
+# pendown()
+# left(90)
+# forward(200)
+# mainloop()
+#bài 6
+from turtle import*
+pencolor('#cf8f03') 
+left(45)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+penup()
+goto(50,0)
+pendown()
+pencolor('#0b2c3c') 
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+mainloop()
